0031f961d0a7a0176a348c984439c6d95103a61d
Preston Darrough <techdarrough@gmail.com>
2021-11-20 12:38:11 -0800

    working counts with table

--- README.md
+++ README.md
@@ -11,9 +11,9 @@ A review activity for the intro full stack course
 - You are a developer working for a pet adoption agency. The team has built an Paw-some website, but now your pesky Product Manager has tasked you with adding the voting functionality for the upcoming Pet of the Month.
 
 # Tasks
-- [ ] Add "Vote" buttons to each pet on the page
-- [ ] When a pet gets a vote, store their score in the leaderboard
-- [ ] Use localStorage to store votes between page visits
+- [ x] Add "Vote" buttons to each pet on the page
+- [x ] When a pet gets a vote, store their score in the leaderboard
+- [ x] Use localStorage to store votes between page visits
 
 ## Bonus Task
 - [ ] Add another button for calculating the favorite pet
--- script.js
+++ script.js
@@ -1,12 +1,31 @@
 // vote count table elements
 
-let voteTableCell0 = document.querySelector('#doggo')
-let voteTableCell1 = document.querySelector('#catto')
-let voteTableCell2 = document.querySelector('#fish')
+let voteTableCell0 = document.querySelector('#doggo-votes');
+let voteTableCell1 = document.querySelector('#catto-votes');
+let voteTableCell2 = document.querySelector('#fish-votes');
 
 
 //count vars here
+if(localStorage.getItem('petVote0') === null){
+    var petVote0 = 0;
+} else {
+    var petVote0 = parseInt(localStorage.getItem('petVote0'));
+    voteTableCell0.textContent = petVote0;
+}
 
+if(localStorage.getItem('petVote1') === null){
+    var petVote1 = 0;
+} else {
+    var petVote1 = parseInt(localStorage.getItem('petVote1'));
+    voteTableCell1.textContent = petVote1;
+}
+
+if(localStorage.getItem('petVote2') === null){
+    var petVote2 = 0;
+} else {
+    var petVote2 = parseInt(localStorage.getItem('petVote2'));
+    voteTableCell2.textContent = petVote2;
+}
 
 // create button add text and style to button
 let petContainers = document.querySelectorAll('.animal');
@@ -18,8 +37,47 @@ let petContainers = document.querySelectorAll('.animal');
     button.setAttribute('class', className);
     button.textContent = 'Vote! for this pet';
     button.style.fontSize = '16px';
-    
+    button.style.margin = '5px'
+   // button.style.center
     petContainers[i].append(button);
  } 
+// Set selector for counts and count click events on buttons
+//buton_0
+let button_0 = document.querySelector('.petVoteButton-0');
 
- 
+button_0.addEventListener("click", function(){
+  //  console.log(e.target)
+  petVote0 = petVote0 + 1
+  voteTableCell0.textContent = petVote0;
+  //set local
+  localStorage.setItem('petVote0', petVote0);
+
+  
+})
+//button_1
+let button_1 = document.querySelector('.petVoteButton-1');
+
+button_1.addEventListener("click", function(){
+    //  console.log(e.target)
+    petVote1 = petVote1 + 1
+    voteTableCell1.textContent = petVote1;
+    //set local
+    localStorage.setItem('petVote1', petVote1);
+  
+    
+  })
+// button_2
+let button_2 = document.querySelector('.petVoteButton-2');
+
+button_2.addEventListener("click", function(){
+    //  console.log(e.target)
+    petVote2 = petVote2 + 1
+    voteTableCell2.textContent = petVote2;
+    //set local
+    localStorage.setItem('petVote2', petVote2);
+  
+    
+  })
+
+//
+let favPetButton = document
